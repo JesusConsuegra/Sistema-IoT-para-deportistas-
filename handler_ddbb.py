8d458a67f6b3736ba5ce0c9ed41ddc1dc0b0ff8c
JairCV <velaidescj@gmail.com>
2020-12-28 11:37:47 -0500

    agregando metodo para mostrar los datos referentes a la sesion guardada en le base de datos

--- handler_ddbb.py
+++ handler_ddbb.py
@@ -2,9 +2,9 @@ from tinydb import TinyDB, where
 
 class handler_ddbb:
 	
-	def agregar_datos(self, estampa, FC, VFC):
+	def agregar_datos(self, estampa, FC, VFC, sesion):
 		db=TinyDB("ddbb.json")
-		db.insert({"estampa":estampa, "FC":FC, "VFC":VFC})
+		db.insert({"estampa":estampa, "FC":FC, "VFC":VFC, "sesion":sesion})
 	
 	def consultar_datos(self):
 		db=TinyDB("ddbb.json")
@@ -15,6 +15,31 @@ class handler_ddbb:
 			cad=cad+"<tr><td>Estampa</td><td>"+str(elem["estampa"])+"</td></tr>"
 			cad=cad+"<tr><td>FC</td><td>"+str(elem["FC"])+"</td></tr>"
 			cad=cad+"<tr><td>VFC</td><td>"+str(elem["VFC"])+"</td></tr>"
+			cad=cad+"<tr><td>Sesion</td><td>"+str(elem["sesion"])+"</td></tr>"
+		cad=cad+"</table>"
+		return cad
+	
+	def consultar_sesion(self, sesion):
+		db=TinyDB("ddbb.json")
+		lista=db.search(where("sesion")==sesion)
+		hr=0
+		rr=0
+		con=0
+		
+		cad="<table border='2'>"
+		for elem in lista:
+			cad=cad+"<tr><td>Estampa</td><td>"+str(elem["estampa"])+"</td></tr>"
+			cad=cad+"<tr><td>FC</td><td>"+str(elem["FC"])+"</td></tr>"
+			cad=cad+"<tr><td>VFC</td><td>"+str(elem["VFC"])+"</td></tr>"
+			cad=cad+"<tr><td>Sesion</td><td>"+str(elem["sesion"])+"</td></tr>"
+			cad=cad+"<tr><td> </td><td>" "</td></tr>"
+			hr=hr+elem["FC"]
+			rr=rr+elem["VFC"]
+			con=con+1
+		
+		cad=cad+"<tr><td>promedio FC</td><td>"+str(hr/con)+"</td></tr>"
+		cad=cad+"<tr><td>promedio VFC</td><td>"+str(rr/con)+"</td></tr>"
+		cad=cad+"<tr><td>Cantidad de datos</td><td>"+str(con)+"</td></tr>"
 		cad=cad+"</table>"
 		return cad
 
