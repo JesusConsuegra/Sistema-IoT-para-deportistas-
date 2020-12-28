a506b0307d04adc73983eec50639cb8684f07838
JairCV <velaidescj@gmail.com>
2020-12-28 09:48:53 -0500

    agregando informacion a la base de datos

--- project.py
+++ project.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from handler_ddbb import handler_ddbb
 import random
+import time
 
 app = Flask(__name__)
+man = handler_ddbb()
 
 @app.route("/")
 def home():
@@ -12,6 +15,8 @@ def home():
 def aleatorio():
 	HR = random.randint(60,100)
 	RR = 60000/HR
+	estampa=time.time()*1000
+	man.agregar_datos(estampa, RR, HR)
 	return	"{\"HR\":"+str(HR)+",\"RR\":"+str(RR)+"}"
 
 if __name__ == "__main__":
